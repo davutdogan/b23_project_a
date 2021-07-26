58fd7d2224ec26e077e6b8a4651306413e0e12a2
davutdogan <davutdogan85@icloud.com>
2021-07-25 23:05:03 -0500

    3rd change

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Added some more thing");
         System.out.println("my second changes");
+        System.out.println("my 3rd changes");
     }
 }
