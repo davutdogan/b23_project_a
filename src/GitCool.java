691b342f2103c2d39bc62ea96cb2d0532fa71e50
davutdogan <davutdogan85@icloud.com>
2021-07-25 22:59:27 -0500

    added github account

--- src/GitCool.java
+++ src/GitCool.java
@@ -1,4 +1,10 @@
 public class GitCool {
     //B23 is learning git
     //B23
+
+    //We have added our new project on github
+    //make sure to click apply and click to save
+    //it
+    //create remote
+    //push
 }
